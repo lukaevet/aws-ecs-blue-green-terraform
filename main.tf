279bf2e94ddceb76e489471989315ec7fa43bfca
Luka Evetovic <lukaevet@gmail.com>
2023-06-01 13:54:51 +0200

    Add CodeDeploy for deployment type

--- main.tf
+++ main.tf
@@ -104,6 +104,14 @@ resource "aws_lb_target_group" "ip-tg" {
   vpc_id      = aws_vpc.main.id
 }
 
+resource "aws_lb_target_group" "ip-tg2" {
+  name        = "ip-tg2"
+  port        = 8080
+  protocol    = "HTTP"
+  target_type = "ip"
+  vpc_id      = aws_vpc.main.id
+}
+
 resource "aws_lb_listener" "listener" {
   load_balancer_arn = aws_lb.elb.arn
   port              = "80"
@@ -112,6 +120,17 @@ resource "aws_lb_listener" "listener" {
   default_action {
     type             = "forward"
     target_group_arn = aws_lb_target_group.ip-tg.arn
+  }
+}
+
+resource "aws_lb_listener" "listener2" {
+  load_balancer_arn = aws_lb.elb.arn
+  port              = "8080"
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.ip-tg2.arn
   }
 }
 
@@ -182,5 +201,157 @@ resource "aws_ecs_service" "ecs-service" {
     subnets = [aws_subnet.public1.id,aws_subnet.public2.id]
     assign_public_ip = true
     security_groups = [aws_security_group.allow_tls.id]
+  }
+  deployment_controller {
+    type = "CODE_DEPLOY"
+  }
+}
+
+/*Code deploy*/
+data "aws_iam_policy_document" "assume_by_codedeploy" {
+  statement {
+    sid     = ""
+    effect  = "Allow"
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type        = "Service"
+      identifiers = ["codedeploy.amazonaws.com"]
+    }
+  }
+}
+
+resource "aws_iam_role" "ecs_task_execution_role" {
+  name = "Code_deploy_ecsTaskExecutionRole"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": ["ecs-tasks.amazonaws.com" ]
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role" "ecs_task_role" {
+  name = "Code_deploy_ecsTaskRole"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": ["ecs.amazonaws.com","ecs-tasks.amazonaws.com"]
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role" "codedeploy" {
+  name               = "codedeploy"
+  assume_role_policy = data.aws_iam_policy_document.assume_by_codedeploy.json
+}
+
+data "aws_iam_policy_document" "codedeploy" {
+  statement {
+    sid    = "AllowLoadBalancingAndECSModifications"
+    effect = "Allow"
+
+    actions = [
+      "iam:PassRole",
+      "ecs:CreateTaskSet",
+      "ecs:DeleteTaskSet",
+      "ecs:DescribeServices",
+      "ecs:UpdateServicePrimaryTaskSet",
+      "elasticloadbalancing:DescribeListeners",
+      "elasticloadbalancing:DescribeRules",
+      "elasticloadbalancing:DescribeTargetGroups",
+      "elasticloadbalancing:ModifyListener",
+      "elasticloadbalancing:ModifyRule",
+      "s3:GetObject"
+    ]
+
+    resources = ["*"]
+  }
+  statement {
+    sid    = "AllowPassRole"
+    effect = "Allow"
+
+    actions = ["iam:PassRole"]
+
+    resources = [
+      aws_iam_role.ecs_task_execution_role.arn,
+      aws_iam_role.ecs_task_role.arn,
+    ]
+  }
+}
+resource "aws_iam_role_policy" "codedeploy" {
+  role   = aws_iam_role.codedeploy.name
+  policy = data.aws_iam_policy_document.codedeploy.json
+}
+
+resource "aws_codedeploy_app" "this" {
+  compute_platform = "ECS"
+  name             = "dev-test-deploy"
+}
+resource "aws_codedeploy_deployment_group" "this" {
+  app_name               = aws_codedeploy_app.this.name
+  deployment_group_name  = "example-deploy-group"
+  deployment_config_name = "CodeDeployDefault.ECSAllAtOnce"
+  service_role_arn       = aws_iam_role.codedeploy.arn
+
+  blue_green_deployment_config {
+    deployment_ready_option {
+      action_on_timeout = "CONTINUE_DEPLOYMENT"
+    }
+
+    terminate_blue_instances_on_deployment_success {
+      action = "TERMINATE"
+      termination_wait_time_in_minutes = 1
+    }
+  }
+
+  ecs_service {
+    cluster_name = aws_ecs_cluster.cluster.name
+    service_name = aws_ecs_service.ecs-service.name
+  }
+
+  deployment_style {
+    deployment_option = "WITH_TRAFFIC_CONTROL"
+    deployment_type   = "BLUE_GREEN"
+  }
+  auto_rollback_configuration {
+    enabled = true
+    events = [ "DEPLOYMENT_FAILURE" ]
+  }
+
+  load_balancer_info {
+    target_group_pair_info {
+      prod_traffic_route {
+        listener_arns = [aws_lb_listener.listener.arn]
+      }
+
+      target_group {
+        name = aws_lb_target_group.ip-tg.name
+      }
+
+      target_group {
+        name = aws_lb_target_group.ip-tg2.name
+      }
+    }
   }
 }
